e3d57eafafbe233b1bcd72476f7a37a6c3863016
lblaszkowski <lblaszkowski@onet.pl>
2019-10-29 19:42:56 +0100

    test verification

--- test_Search_hotel.py
+++ test_Search_hotel.py
@@ -13,14 +13,34 @@ driver.find_element_by_name("checkout").send_keys("29/10/2019")
 driver.find_element_by_id("travellersInput").click()
 driver.find_element_by_id("adultInput").clear()
 driver.find_element_by_id("adultInput").send_keys("4")
-driver.find_element_by_id("childInput").clear()
-driver.find_element_by_id("childInput").send_keys("4")
 driver.find_element_by_xpath("//button[text()=' Search']").click()
-# //h4[contains(@class,'list_title')]//b
+
+
 hotels = driver.find_elements_by_xpath("//h4[contains(@class,'list_title')]//b")
-hotel_name = [hotel.get_attribute("textContent") for hotel in hotels]
-for name in hotel_name:
+hotel_names = [hotel.text for hotel in hotels]
+for name in hotel_names:
     print("Hotel name: " + name)
+    print(len(hotels))
+    print("test - 1")
+
+print("test - 2")
+
+
+# prices = driver.find_elements_by_xpath("//div[contains(@class,'price_tab')]//b")
+# price_values = [price.get_attribute("textContent") for price in prices]
+# for price in price_values:
+#     print("Cena to: " + price)
+
+
+# assert hotel_names[0] == 'Jumeirah Beach Hotel'
+# assert hotel_names[1] == 'Oasis Beach Tower'
+# assert hotel_names[2] == 'Rose Rayhaan Rotana'
+# assert hotel_names[3] == 'Hyatt Regency Perth'
+
+# assert price_values[0] == '$22'
+# assert price_values[1] == '$50'
+# assert price_values[2] == '$80'
+# assert price_values[3] == '$150'
 
 driver.close()
 driver.quit()
